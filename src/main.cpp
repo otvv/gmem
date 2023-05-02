ded2e01077377391385874a624cc5fbcf4e49d8e
Otavio <suizld@pm.me>
2023-05-02 12:55:55 -0300

    update: removed left over code from prev. testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "gmem/gmem.h"
 
-inline std::uint32_t LOCAL_PLAYER_ADDR = 0x20c1138;
-inline std::int32_t HEALTH = 0x43c;
+inline std::uint32_t BOGUS_ADDR = 0xDEADBEEF;
+inline std::int32_t SOMETHING = 0xFFF;
 
 int main()
 {
@@ -11,10 +11,10 @@ int main()
   auto base_addr = gmem::process::get_base_addr("r5apex.exe", pid);
   std::cout << "[gmem-test] - module base addr: 0x" << std::hex << base_addr << std::endl;
 
-  auto local_player = gmem::process::read_mem<std::uint32_t>(base_addr + LOCAL_PLAYER_ADDR);
+  auto local_player = gmem::process::read_mem<std::uint32_t>(base_addr + BOGUS_ADDR);
   std::cout << "[gmem-test] - local player ptr: 0x" << std::hex << local_player << std::endl;
 
-  auto health = gmem::process::read_mem<std::int32_t>(local_player + HEALTH);
+  auto health = gmem::process::read_mem<std::int32_t>(local_player + SOMETHING);
   std::cout << "[gmem-test] - local player health: " << std::dec << health << std::endl;
 
   return 0;
